8eb79d6ba2503674932c16d610cbd49562145556
NV1591 <nakkul.verma15@gmail.com>
2014-02-09 18:40:31 +0530

    Models updated till Context

---
+++ ChaiShai/ChaiShai/Model/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChaiShai.Model
+{
+    public class Category
+    {
+        [ScaffoldColumn(false)]
+        public int CategoryID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string CategoryName { get; set; }
+
+        [Required, StringLength(100000), Display(Name = "Category Description")]
+        public string CategoryDescription { get; set; }
+
+        public virtual ICollection<Products> Products { get; set;}
+    }
+}
